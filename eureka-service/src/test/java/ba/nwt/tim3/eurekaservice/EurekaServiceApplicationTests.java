9380540c3d8a279a4ee7ae5985fe1b954f41254f
Ahmedin Hasanovic <hasanovicahmedin6@gmail.com>
2023-04-09 22:27:13 +0200

    Dodan eureka-service

---
+++ eureka-service/src/test/java/ba/nwt/tim3/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.nwt.tim3.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
