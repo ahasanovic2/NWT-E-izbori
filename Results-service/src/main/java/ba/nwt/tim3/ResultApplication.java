0ed968377a5f2a873333eabd311262a0664b2c48
Ahmedin Hasanovic <hasanovicahmedin6@gmail.com>
2023-05-02 16:53:12 +0200

    Implementirana centralizirana konfiguracija

--- Results-service/src/main/java/ba/nwt/tim3/ResultApplication.java
+++ Results-service/src/main/java/ba/nwt/tim3/ResultApplication.java
@@ -1,9 +1,13 @@
 package ba.nwt.tim3;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ResultApplication implements CommandLineRunner {
@@ -17,5 +21,18 @@ public class ResultApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		resultService.createRows();
+	}
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+	@Value("${message:Hello default}")
+	private String message;
+
+	@RequestMapping("/message")
+	String getMessage() {
+		return this.message;
 	}
 }
