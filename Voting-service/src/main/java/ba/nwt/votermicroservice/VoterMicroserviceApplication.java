486b997ac7343237bd8d80b1287c82bddfe258c3
Ahmedin Hasanovic <hasanovicahmedin6@gmail.com>
2023-05-02 15:59:21 +0200

    Centralizirani config za voting servis

--- Voting-service/src/main/java/ba/nwt/votermicroservice/VoterMicroserviceApplication.java
+++ Voting-service/src/main/java/ba/nwt/votermicroservice/VoterMicroserviceApplication.java
@@ -1,9 +1,13 @@
 package ba.nwt.votermicroservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class VoterMicroserviceApplication implements CommandLineRunner {
@@ -17,5 +21,18 @@ public class VoterMicroserviceApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         voterManagementService.createRows();
+    }
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+    @Value("${message:Hello default}")
+    private String message;
+
+    @RequestMapping("/message")
+    String getMessage() {
+        return this.message;
     }
 }
