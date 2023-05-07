bcf939ba1a1415d418babfdcdd135c1c560ba0e9
Ahmedin Hasanovic <hasanovicahmedin6@gmail.com>
2023-05-07 10:38:10 +0200

    added dependencies

--- system-events/src/main/java/ba/nwt/tim3/systemevents/SystemEventsApplication.java
+++ system-events/src/main/java/ba/nwt/tim3/systemevents/SystemEventsApplication.java
@@ -1,12 +1,15 @@
 package ba.nwt.tim3.systemevents;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import org.lognet.springboot.grpc.GRpcService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SystemEventsApplication {
-
     public static void main(String[] args) {
+        Server server = ServerBuilder.forPort(8800).addService(new EventImpl()).build();
         SpringApplication.run(SystemEventsApplication.class, args);
     }
 
