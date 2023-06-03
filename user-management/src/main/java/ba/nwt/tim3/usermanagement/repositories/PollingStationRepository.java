8481b8d595dcc773764dea78bc11fd17fcbc6c71
Ahmedin Hasanović <ahasanovic2@etf.unsa.ba>
2023-06-03 19:05:33 +0200

    user management
    izbrisan user management servis jer auth servis sve radi sad

--- user-management/src/main/java/ba/nwt/tim3/usermanagement/repositories/PollingStationRepository.java
+++
@@ -1,9 +0,0 @@
-package ba.nwt.tim3.usermanagement.repositories;
-
-import ba.nwt.tim3.usermanagement.entities.PollingStation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PollingStationRepository extends JpaRepository<PollingStation,Long> {
-}
