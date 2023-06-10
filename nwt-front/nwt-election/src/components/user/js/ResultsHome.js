9b86f98a2bf0f8de6ffa09fe75711ddbceaeebc1
Ahmedin Hasanović <ahasanovic2@etf.unsa.ba>
2023-06-10 19:57:38 +0200

    results frontend
    zavrsen results na frontend stranica, potrebno uvezati jer dugmad ne rade na njoj za rutiranje i potrebno je dodati na ostale stranice

--- nwt-front/nwt-election/src/components/user/js/ResultsHome.js
+++ nwt-front/nwt-election/src/components/user/js/ResultsHome.js
@@ -18,10 +18,11 @@ const ResultsHome = () => {
 
     const handleResultsOption = async (option) => {
         const token = localStorage.getItem('access_token');
+        const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:8080';
         switch (option) {
             case 1:
                 try {
-                    const response = await fetch(`http://localhost:8080/result-microservice/results/full-election?electionName=${election}`, {
+                    const response = await fetch(`${BASE_URL}/result-microservice/results/full-election?electionName=${election}`, {
                         headers: {
                             'Authorization': `Bearer ${token}`
                         }
@@ -46,17 +47,33 @@ const ResultsHome = () => {
                 
                 break;
             case 2:
-                const response2 = await fetch(`http://localhost:8080/result-microservice/results/election/pollingStation?electionName=${election}&pollingStationName=${pollingStation}`, {
-                    headers: {
-                        'Authorization': `Bearer ${token}`
+                try {
+                    const response2 = await fetch(`${BASE_URL}/result-microservice/results/election/pollingStation?electionName=${election}&pollingStationName=${pollingStation}`, {
+                        headers: {
+                            'Authorization': `Bearer ${token}`
+                        }
+                    });
+                    if (!response2.ok) { // check if HTTP status is not ok (200)
+                        const errorData = await response2.json();
+                        setError1(errorData.message);
+                        return;
                     }
-                });
-                const data2 = await response2.json();
-                setResults(data2);
+                    const data = await response2.json();
+                    setResults(data);
+                    setError1('');
+                    setError2('');
+                    setError3('');
+                    setError4('');
+                    setError5('');
+                    setError6('');
+                }
+                catch (err) {
+                    setError2('An unexpected error occured');
+                }
                 break;
             case 3:
                 try {
-                    const response3 = await fetch(`http://localhost:8080/result-microservice/results/election/candidate?electionName=${election}&candidateFirstName=${candidateFirstName}&candidateLastName=${candidateLastName}`, {
+                    const response3 = await fetch(`${BASE_URL}/result-microservice/results/election/candidate?electionName=${election}&candidateFirstName=${candidateFirstName}&candidateLastName=${candidateLastName}`, {
                         headers: {
                             'Authorization': `Bearer ${token}`
                         }
@@ -78,6 +95,84 @@ const ResultsHome = () => {
 
                 } catch (err) {
                     setError3('An unexpected error occurred');
+                }
+                break;
+            case 4:
+                try {
+                    const response4 = await fetch(`${BASE_URL}/result-microservice/results/election/list?electionName=${election}&listaName=${list}`, {
+                        headers: {
+                            'Authorization': `Bearer ${token}`
+                        }
+                    });
+                    
+                    if (!response4.ok) { // check if HTTP status is not ok (200)
+                        const errorData = await response4.json();
+                        setError4(errorData.message);
+                        return;
+                    }
+                    const data4 = await response4.json();
+                    setResults([data4]); // encapsulate data in array
+                    setError1('');
+                    setError2('');
+                    setError3('');
+                    setError4('');
+                    setError5('');
+                    setError6('');
+
+                } catch (err) {
+                    setError4('An unexpected error occurred');
+                }
+                break;
+            case 5:
+                try {
+                    const response5 = await fetch(`${BASE_URL}/result-microservice/results/election/pollingStation/candidate?electionName=${election}&candidateFirstName=${candidateFirstName}&candidateLastName=${candidateLastName}&pollingStationName=${pollingStation}`, {
+                        headers: {
+                            'Authorization': `Bearer ${token}`
+                        }
+                    });
+                    
+                    if (!response5.ok) { // check if HTTP status is not ok (200)
+                        const errorData = await response5.json();
+                        setError5(errorData.message);
+                        return;
+                    }
+                    const data5 = await response5.json();
+                    setResults([data5]); // encapsulate data in array
+                    setError1('');
+                    setError2('');
+                    setError3('');
+                    setError4('');
+                    setError5('');
+                    setError6('');
+
+                } catch (err) {
+                    setError5('An unexpected error occurred');
+                }
+                break;
+            case 6:
+                try {
+                    const response6 = await fetch(`${BASE_URL}/result-microservice/results/election/pollingStation/list?electionName=${election}&listaName=${list}&pollingStationName=${pollingStation}`, {
+                        headers: {
+                            'Authorization': `Bearer ${token}`
+                        }
+                    });
+                    
+                    if (!response6.ok) { // check if HTTP status is not ok (200)
+                        const errorData = await response6.json();
+                        setError6(errorData.message);
+                        return;
+                    }
+                    const data6 = await response6.json();
+                    setResults([data6]); // encapsulate data in array
+                    setError1('');
+                    setError2('');
+                    setError3('');
+                    setError4('');
+                    setError5('');
+                    setError6('');
+
+                } catch (err) {
+                    setError6('An unexpected error occurred');
                 }
                 break;
             default:
@@ -143,11 +238,15 @@ const ResultsHome = () => {
                 <button onClick={() => handleResultsOption(1)}>Get full results for election</button>
                 {error1 && <p className="error-message">{error1}</p>}
                 <button onClick={() => handleResultsOption(2)}>Get results for election at certain polling station</button>
+                {error2 && <p className="error-message">{error2}</p>}
                 <button onClick={() => handleResultsOption(3)}>Get full results for one candidate at certain election</button>
                 {error3 && <p className="error-message">{error3}</p>}
-                <button >Get full results for one list at certain election</button>
-                <button >Get results for one candidate at certain election at certain polling station</button>
-                <button >Get results for one list at certain election at certain polling station</button>
+                <button onClick={() => handleResultsOption(4)}>Get full results for one list at certain election</button>
+                {error4 && <p className="error-message">{error4}</p>}
+                <button onClick={() => handleResultsOption(5)}>Get results for one candidate at certain election at certain polling station</button>
+                {error5 && <p className="error-message">{error5}</p>}
+                <button onClick={() => handleResultsOption(6)}>Get results for one list at certain election at certain polling station</button>
+                {error6 && <p className="error-message">{error6}</p>}
             </div>
             <div className='results-table'>
                 <table>
