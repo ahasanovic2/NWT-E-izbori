806fd8555e277d4c3bd67e52f98fdd2df368f114
Ahmedin Hasanović <ahasanovic2@etf.unsa.ba>
2023-06-12 17:50:39 +0200

    obrisano nepotrebno

--- notification-service/src/main/java/ba/nwt/tim3/notificationmicroservices/management/repositories/PollingStationRepository.java
+++
@@ -1,8 +0,0 @@
-package ba.nwt.tim3.notificationmicroservices.management.repositories;
-
-import ba.nwt.tim3.notificationmicroservices.management.models.PollingStation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PollingStationRepository extends JpaRepository<PollingStation, Long> {
-
-}
--- notification-service/src/main/java/ba/nwt/tim3/notificationmicroservices/management/repositories/ResultRepository.java
+++
@@ -1,7 +0,0 @@
-package ba.nwt.tim3.notificationmicroservices.management.repositories;
-
-import ba.nwt.tim3.notificationmicroservices.management.models.Result;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResultRepository extends JpaRepository<Result,Long> {
-}
